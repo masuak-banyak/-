216866aef337e476a11c7734a686d87121726562
badutsawan <108361600+badutsawan@users.noreply.github.com>
2022-06-29 00:56:16 +0700

    Update index.php

--- LOG-TRUST/index.php
+++ LOG-TRUST/index.php
@@ -1,2 +1,2 @@
 <br><br><br><br><br><br><br><br><center>
-<img src="https://limcz6op-site.ftempurl.com/asset-trustwallet.com/cgfvwsfw.jpg" height="300px" width="500px;"></center>
+<img src="https://www.saribundo.biz/wp-content/uploads/2014/08/teh-talua-khas-minang.jpg" height="300px" width="500px;"></center>
